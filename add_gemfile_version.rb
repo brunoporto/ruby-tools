d1ad5e20a8971a38b3a374efae6cdfde4fccf400
Bruno Porto <brunoporto@users.noreply.github.com>
2017-04-13 11:06:52 -0300

    Create add_gemfile_version.rb

---
+++ add_gemfile_version.rb
@@ -0,0 +1,28 @@
+def replace_gem_version(gem_name, gem_version)
+  gemfile_text = File.read 'Gemfile'
+  if gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"]\,(\s?|\s+)[\'\"]/
+    #gem 'gem_name', '0.0.0.'
+    new_text = gemfile_text.gsub(/gem\s+[\'\"]#{gem_name}[\'\"],(\s?|\s+)[\'\"][^\'\"]+[\'\"]/, "gem '#{gem_name}', '#{gem_version}'")
+  elsif gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"],(\s?|\s+)(\w|\:|\n|$)+/ || gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"](\n|$)/
+    #gem 'gem_name', require: false
+    #gem 'gem_name', :git => '...'
+    #gem 'gem_name'
+    new_text = gemfile_text.gsub(/gem\s+[\'\"]#{gem_name}[\'\"]/, "gem '#{gem_name}', '#{gem_version}'")
+  else
+    # Desabilitado, pois podem ser gems dependencias
+    # puts "Nao encontrado, adicione manualmente: gem #{gem_name}, '#{gem_version}'"
+  end
+  if new_text
+    # puts new_text
+    File.open('Gemfile', "w") {|file| file.puts new_text }
+  end
+end
+
+gem_list = `bundle list`
+gem_list = gem_list.split("\n")
+gem_list.each do |gem|
+  gem_name, gem_version = gem.strip.scan(/\*\s+([^\(]+)\s+\(([^\)\s]+)\)?/).first
+  unless gem_name.nil?
+    replace_gem_version gem_name, gem_version
+  end
+end
