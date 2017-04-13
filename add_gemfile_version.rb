262584bfd651ec1d056ae424534880e7dbc8e839
Bruno Porto <brunoporto@users.noreply.github.com>
2017-04-13 11:19:20 -0300

    Update add_gemfile_version.rb

--- add_gemfile_version.rb
+++ add_gemfile_version.rb
@@ -3,9 +3,9 @@ def replace_gem_version(gem_name, gem_version)
   if gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"]\,(\s?|\s+)[\'\"]/
     #gem 'gem_name', '0.0.0.'
     new_text = gemfile_text.gsub(/gem\s+[\'\"]#{gem_name}[\'\"],(\s?|\s+)[\'\"][^\'\"]+[\'\"]/, "gem '#{gem_name}', '#{gem_version}'")
-  elsif gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"],(\s?|\s+)(\w|\:|\n|$)+/ || gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"](\n|$)/
-    #gem 'gem_name', require: false
-    #gem 'gem_name', :git => '...'
+  elsif gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"],(\s?|\s+)(:?group|:?require|:?engine|:?engine_version)+/ || gemfile_text =~ /gem\s+[\'\"]#{gem_name}[\'\"](\n|$)/
+    #gem 'gem_name', require: false  (:require =>)
+    #gem 'gem_name', group: :test (:group => :test)
     #gem 'gem_name'
     new_text = gemfile_text.gsub(/gem\s+[\'\"]#{gem_name}[\'\"]/, "gem '#{gem_name}', '#{gem_version}'")
   else
